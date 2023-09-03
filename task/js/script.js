07acf30bcd67ea8aba0ff6eb9b43a5c2bd5860eb
HughPullman <hughpullman54@gmail.com>
2023-09-03 21:38:21 +0100

    Finished Task
    Finished initial task

---
+++ task/js/script.js
@@ -0,0 +1,83 @@
+$('#submitEarthquakeInfo').click(function() {
+
+    $.ajax({
+        url: "/task/php/getEarthquakeInfo.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            north: $('#inputNorth').val(),
+            south: $('#inputSouth').val(),
+            east: $('#inputEast').val(),
+            west: $('#inputWest').val()
+        },
+        success: function(result) {
+
+            console.log(JSON.stringify(result));
+
+            if(result.status.name == "ok") {
+
+                $('#txtDateTime').html(result['data'][0]['datetime'])
+                $('#txtDepth').html(result['data'][0]['depth'])
+                $('#txtLng').html(result['data'][0]['lng'])
+                $('#txtLat').html(result['data'][0]['lat'])
+                $('#txtMagnitude').html(result['data'][0]['magnitude'])
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown){
+
+        } 
+    });
+});
+
+$('#submitOceanInfo').click(function() {
+
+    $.ajax({
+        url: "/task/php/getOceanInfo.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            lat: $('#inputLatitude').val(),
+            lng: $('#inputLongitude').val()
+        },
+        success: function(result) {
+            console.log(JSON.stringify(result));
+
+            if(result.status.name == "ok"){
+
+                $('#txtOcean').html(result['data'][0]['name'])
+            }
+        }
+    });
+});
+
+$('#submitWeatherInfo').clikc(function() {
+
+    $.ajax({
+        url: "/task/php/getWeatherInfo.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            north: $('#inputNorthW').val(),
+            south: $('#inputSouthW').val(),
+            east: $('#inputEastW').val(),
+            west: $('#inputWestW').val()
+        },
+        success: function(result) {
+
+            console.log(JSON.stringify(result));
+
+            if(result.status.name == "ok") {
+
+                $('#txtTemperature').html(result['data'][0]['temperature'])
+                $('#txtHumidity').html(result['data'][0]['humidity'])
+                $('#txtLng').html(result['data'][0]['lng'])
+                $('#txtLat').html(result['data'][0]['lat'])
+                $('#txtWindSpeed').html(result['data'][0]['windSpeed'])
+                $('#txtStationName').html(result['data'][0]['stationName'])
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown){
+
+        } 
+    });    
+});
